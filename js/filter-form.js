e1244e18ee210958a199bd73f494ceb319420e7b
KiraShubik <kira.shubik@gmail.com>
2015-09-25 13:21:54 +0300

    cookies (filter-form)

--- js/filter-form.js
+++ js/filter-form.js
@@ -1,13 +1,16 @@
 (function() {
-  var uploadForm = document.forms['upload-select-image'];
-  var resizeForm = document.forms['upload-resize'];
-  var filterForm = document.forms['upload-filter'];
+  var uploadForm = document.forms['upload-select-image']; //форма выбора изображения - 1 шаг
+  var resizeForm = document.forms['upload-resize']; //форма редактирования изображения - 2 шаг
+  var filterForm = document.forms['upload-filter']; //форма выбора фильтра - 3 шаг
 
-  var previewImage = filterForm.querySelector('.filter-image-preview');
-  var prevButton = filterForm['filter-prev'];
-  var selectedFilter = filterForm['upload-filter'];
-
+  var previewImage = filterForm.querySelector('.filter-image-preview'); //загруженное изображение
+  var prevButton = filterForm['filter-prev']; // Кнопка отправки формы
+  var selectedFilter = filterForm['upload-filter']; // Коллекция фильтров
+  
   var filterMap;
+
+  // Функция добавляет коллекцию фильтров в переменную filterMap, объявленную выше
+  // Функция ограничивает максимальную ширину img до 582px и добавляет изображению значение выбранного css-фильтра
 
   function setFilter() {
     if (!filterMap) {
@@ -17,16 +20,15 @@
         'sepia': 'filter-sepia'
       };
     }
-
     previewImage.className = 'filter-image-preview' + ' ' + filterMap[selectedFilter.value];
   };
 
+  // Цикл перебирает массив фильтров и в случае выбранного фильтра вызывает фунццию setFilter, которая добавляет нужный класс для применения css-фильтра
   for (var i = 0, l = selectedFilter.length; i < l; i++) {
     selectedFilter[i].onchange = function(evt) {
       setFilter();
     }
   }
-
   prevButton.onclick = function(evt) {
     evt.preventDefault();
 
@@ -37,10 +39,20 @@
 
   filterForm.onsubmit = function() {
     evt.preventDefault();
+    // Запись в cookie.
+    docCookies.setItem('upload-filter', selectedFilter.value);
 
     uploadForm.classList.remove('invisible');
     filterForm.classList.add('invisible');
+    
+    //filterForm.submit();
+
+  }
+  // Восстановление cookie
+  if (docCookies.hasItem('upload-filter')) {
+    selectedFilter.value = docCookies.getItem('upload-filter');
   }
 
   setFilter();
+  
 })();
